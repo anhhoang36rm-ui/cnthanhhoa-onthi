2dfa4d67a392f012a61f1b3a37b4494ba7e1d83f
anhhoang36rm-ui <anhhoang36rm@gmail.com>
2026-09-06 23:40:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1790,7 +1790,7 @@ input[type="text"] {padding:5px; min-width:140px; font-size:12.5px;}
 <body>
 <div class="container">
 <div class="admin-topbar">
-  <h2>Quản lý đăng ký</h2>
+  <h2>Quản lý tài khoản</h2>
   <button id="adminHamburgerBtn" class="admin-hamburger-btn" aria-label="Menu">☰ Menu</button>
   <div id="adminHamburgerMenu" class="admin-hamburger-menu hidden">
     <a class="admin-menu-item" href="/admin/login_logs">🕘 Xem log đăng nhập</a>
